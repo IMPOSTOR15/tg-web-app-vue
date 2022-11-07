aead2755f0c6998b456a1b635a294c5b58fc330f
Andrey Dagaev <dandr212@gmail.com>
2022-11-07 20:32:28 +0300

    some

---
+++ src/components/button.vue
@@ -0,0 +1,20 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.button {
+  padding: 10px 15px;
+  background: var(--tg-theme-bg-color);
+  color: var(--tg-theme-text-color);
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <Button @click="onClose">Закрыть</Button>
+    <span>
+        {tg.initDataUnsafe?.user?.username}
+    </span>
+  </div>  
+</template>
+
+<script>
+const tg = window.Telegram.WebApp;
+import Button from "./Button";
+import {useTelegram} from "../../hooks/useTelegram";
+export default {
+  components: {
+    Button
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    onClose() {
+      tg.close()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    padding: 0 20px;
+}
+
+.username {
+    margin-left: auto;
+}
+</style>
